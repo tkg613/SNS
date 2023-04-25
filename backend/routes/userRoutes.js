2b2493220d189644805f48eb89fa8ec0cb0f5edd
tkg613 <lyntallow@mail.com>
2023-04-25 16:39:42 +0900

    Complete auth functionalities

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {registerUser, loginUser} = require('../controllers/userController')
+const {registerUser, loginUser, getMe} = require('../controllers/userController')
 const router = express.Router()
+const {protect} = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
-
 router.post('/login', loginUser)
+router.get('/me', protect, getMe)
 
 module.exports = router
